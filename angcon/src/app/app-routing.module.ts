9f7d647b28d3295bb500af443679ea0bd7df2494
Joshatrevature <87381776+Joshatrevature@users.noreply.github.com>
2021-09-14 19:13:06 -0700

    Angular file structure fixed
    fixed file structure other than user

--- angcon/src/app/app-routing.module.ts
+++ angcon/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
