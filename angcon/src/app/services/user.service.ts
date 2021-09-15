cfdc0c2e1999c754ced433f0c1368eb7efa5a5bd
FosterVanDoren <87378404+FosterVanDoren@users.noreply.github.com>
2021-09-14 21:42:42 -0400

    Added services folder and card template
    Added a service to use the RAWG API and a card template. Also moved user service into the services folder

--- angcon/src/app/services/user.service.ts
+++ angcon/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { IUser } from './user';
 import { Observable } from 'rxjs';
+import { IUser } from '../user';
 
 
 @Injectable({
