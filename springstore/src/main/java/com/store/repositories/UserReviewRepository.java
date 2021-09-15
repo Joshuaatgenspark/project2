c38fa03c709727a3280addd28cf954ad9a8abcd4
nkRev <nicky.kayyarath@revature.net>
2021-09-15 00:31:46 -0400

    Spring Services for user review service and user service are "finished" may need to add more implementation depending on what we need. updated User and UserReviews entities.

--- springstore/src/main/java/com/store/repositories/UserReviewRepository.java
+++ springstore/src/main/java/com/store/repositories/UserReviewRepository.java
@@ -1,18 +1,11 @@
 package com.store.repositories;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.store.entity.UserReviews;
 
 @Repository
 public interface UserReviewRepository extends JpaRepository<UserReviews, Long>{
-    //have to finish the query to get a list of reviews that a User has done.
-    @Query(
-        value = "select * from ",
-        nativeQuery=true
-    )
-	public List<UserReviews> getReviewsByUserName(String userName);
+
 }
