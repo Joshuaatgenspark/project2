777c7ff8917f92f13e0c5b43847664161d7f9311
Joshatrevature <87381776+Joshatrevature@users.noreply.github.com>
2021-09-15 08:00:29 -0700

    Carousel Works
    The carousel works and just needs some style work

--- angcon/src/app/services/rawg-data.service.ts
+++ angcon/src/app/services/rawg-data.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class RawgDataService {
 
   private ENDPOINTS = {
-    GAME_LIST_URL: 'https://api.rawg.io/api/games?key=6d9368c98b03417d9a0cab226f150dc0'
+    GAME_LIST_URL: 'https://rawg.io/api/games?key=6d9368c98b03417d9a0cab226f150dc0&page=1&page_size=5&ordering=-released&stores=11'
   }
 
   constructor(private http: HttpClient) { }
