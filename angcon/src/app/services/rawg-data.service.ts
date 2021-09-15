f39c179067bda559d2e9b2252eba6bbd3166625e
FosterVanDoren <87378404+FosterVanDoren@users.noreply.github.com>
2021-09-15 10:18:31 -0400

    Fixed cards
    cards now display properly

--- angcon/src/app/services/rawg-data.service.ts
+++ angcon/src/app/services/rawg-data.service.ts
@@ -8,11 +8,14 @@ import { Observable } from 'rxjs';
 export class RawgDataService {
 
   private ENDPOINTS = {
-    GAME_LIST_URL: 'https://rawg.io/api/games?page=1&page_size=20&key=6d9368c98b03417d9a0cab226f150dc0'
+    GAME_LIST_URL: 'https://rawg.io/api/games?key=6d9368c98b03417d9a0cab226f150dc0&page=1&page_size=20'
   }
 
   constructor(private http: HttpClient) { }
-
+  /**
+   * 
+   * @returns observable of one page of games populated with 20 games
+   */
   getGames(): Observable<any>{
     return this.http.get<any>(this.ENDPOINTS.GAME_LIST_URL);
   }
