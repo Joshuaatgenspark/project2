0379fbed50ee8288ca0037a3ff10baef755fd1df
TroyMorrow <87380265+TroyMorrow@users.noreply.github.com>
2021-09-17 09:34:14 -0700

    Updated Home page format

--- angcon/src/app/app-routing.module.ts
+++ angcon/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { GameCardsComponent } from './components/game-cards/game-cards.component';
 
 
+
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component:HomeComponent},
